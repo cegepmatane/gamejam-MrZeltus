a191e1ea2a90dfb6f2380817991dd93259129070
Matteo <delutiismatteo@gmail.com>
2020-12-06 12:13:24 -0500

    Ajout Ecran de victoire

--- Assets/Scripts/Joueur/VieHUD.cs
+++ Assets/Scripts/Joueur/VieHUD.cs
@@ -9,7 +9,6 @@ public class VieHUD : MonoBehaviour
 {
     public static VieHUD instance;
     public Text m_TextCollectible;
-    public Text m_WinText;
     public Slider slider;
     int a_Score;
 
@@ -18,21 +17,13 @@ public class VieHUD : MonoBehaviour
         if (instance == null)
             instance = this;
 
-        m_WinText.text = "";
         m_TextCollectible.text = "";
     }
 
-    //TODO texte victoire
     public void ChangeScore(int a_CollectableValue)
     {
         a_Score += a_CollectableValue;
         m_TextCollectible.text = ": " + a_Score.ToString();
-
-       /* if (a_Score == 11)
-        {
-            m_WinText.text = "VICTOIRE";
-
-        }*/
     }
 
     public void SetVie(int vie)
