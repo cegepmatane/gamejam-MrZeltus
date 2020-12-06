6ea842da6e30c2c146f25f0791688b0e1af5bbb2
Nicolas Rimbault <nicolas@rimbault.org>
2020-12-05 19:39:42 -0500

    ajout menu game over

---
+++ Assets/Scripts/Menu/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField] private GameObject canvas;
+    [SerializeField] public bool isDead;
+    public void Update()
+    {
+        if (isDead == true)
+        {
+            GameOverActivate();
+        }
+    }   
+
+    void GameOverActivate()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        canvas.SetActive(true);
+    }
+}
