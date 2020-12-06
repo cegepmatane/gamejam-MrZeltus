c178667b4a9af8065724f73b58319fb9a6d86037
CGMATANE\1931553 <romainover51@gmail.com>
2020-12-06 17:50:24 -0500

    Validation des Fichier

---
+++ Assets/Scripts/Menu/Crosshair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public GameObject image;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pz = Input.mousePosition;
+        image.transform.position = pz;
+    }
+}
